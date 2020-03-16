cbf55994512268cba94b7c57c4f508508059f3e1
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-16 11:20:19 -0400

    Add project files.

---
+++ Parcial2_Darwin_Lantigua/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial2_Darwin_Lantigua.DAL
+{
+    class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source: Database\Parcial2.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 1, Descripcion = "Chocolate", Precio = 100 });
+        }
+    }
+}
